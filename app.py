9029e0e717bc5a1ef7163205638463d571201e68
nndia-gif <aryadisop@gmail.com>
2025-09-03 21:11:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,6 @@ def index():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))  # Railway inject PORT
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
+
 
